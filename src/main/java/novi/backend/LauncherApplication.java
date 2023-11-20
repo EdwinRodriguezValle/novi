083b77dee283c6bda6cc221228fc19883389f807
edwin <rodriguez.valle.edwin@gmail.com>
2023-11-20 12:22:01 +0100

    adding layers

---
+++ src/main/java/novi/backend/LauncherApplication.java
@@ -0,0 +1,13 @@
+package novi.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LauncherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LauncherApplication.class, args);
+	}
+
+}
